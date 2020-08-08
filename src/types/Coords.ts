bbb8c5f0f1236de1b9e65d33158627f878f14583
Tyler Menezes <github@xanadu.ws>
2020-08-08 13:03:13 -0400

    Coords has an undocumented constructor

--- src/types/Coords.ts
+++ src/types/Coords.ts
@@ -1,12 +1,13 @@
 import Direction from './Direction';
 
 export default class Coords {
+  constructor(x: number, y: number) {}
   x: number;
   y: number;
   distanceTo(other: Coords) { return 0; }
   walkingDistanceTo(other: Coords) { return 0; }
   directionTo(other: Coords) { return new Direction(); }
-  add(other: Coords | Direction) { return new Coords(); }
-  sub(other: Coords | Direction) { return new Coords(); }
-  mul(other: Coords | Direction) { return new Coords(); }
+  add(other: Coords | Direction) { return new Coords(0, 0); }
+  sub(other: Coords | Direction) { return new Coords(0, 0); }
+  mul(other: Coords | Direction) { return new Coords(0, 0); }
 }
